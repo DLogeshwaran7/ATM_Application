7ee7258267e6a25cb3d85692c93a18d34219b346
Logeshwaran D <dlogeshwaran7@gmail.com>
2026-02-11 21:11:39 +0530

    Create ATM_Application.java

---
+++ ATM_Application.java
@@ -0,0 +1,15 @@
+
+
+public class ATM_Application {
+
+	// ---------------- MAIN CLASS ----------------
+
+	    public static void main(String[] args) {
+
+	        ATM atm = new ATM();
+
+	        atm.login();
+	    }
+	}
+
+
